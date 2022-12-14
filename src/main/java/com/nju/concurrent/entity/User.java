9a207afe340fc154715a0b89f0357b5df658eb62
邱依良 <553579048@qq.com>
2022-12-14 20:40:58 +0800

    完成第二章 线程安全

---
+++ src/main/java/com/nju/concurrent/entity/User.java
@@ -0,0 +1,11 @@
+package com.nju.concurrent.entity;
+
+/**
+ * @description
+ * @date:2022/12/14 15:12
+ * @author: qyl
+ */
+public class User {
+    public String name;
+    public Integer age;
+}
