48b791d092eba093a2690c43a4f374f06061805f
邱依良 <553579048@qq.com>
2022-12-17 00:44:18 +0800

    继续第三章学习

--- src/main/java/com/nju/concurrent/entity/User.java
+++ src/main/java/com/nju/concurrent/entity/User.java
@@ -5,7 +5,32 @@ package com.nju.concurrent.entity;
  * @date:2022/12/14 15:12
  * @author: qyl
  */
-public class User {
+public class User implements Comparable<User>{
     public String name;
-    public Integer age;
+    public int age;
+
+    public User(){}
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        if (o.age == age){
+            return name.compareTo (o.name);
+        }
+        return age - o.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode ( );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals (obj);
+    }
 }
